033474259fdad65c13be5cdf304e3dd56d150ed3
Rayden <ray_chris@htomail.it>
2019-05-21 22:12:49 +0200

    commiting new project Contacts

---
+++ public/myScript.js
@@ -0,0 +1,12 @@
+
+//if (x===undefined)
+//    x = "nessun contatto";
+  //else
+    //x = "ciao";
+
+function addFunction (){
+    var x;
+    x.name = document.getElementById("nameContact").value;
+    document.getElementById("demo").innerHTML = x.name;
+}
+
