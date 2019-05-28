3287169526f09f5ce26231e1798f085a79dfdc81
Ray <ray_chris@hotmail.it>
2019-05-28 17:16:48 +0200

    reorder card data and added shadow under the card

--- myScript.js
+++ myScript.js
@@ -16,6 +16,7 @@
      this.pButtonX = frmButtonX;
      this.pButtonOK = frmButtonOK;
      this.contact = [];
+     
      //this.pFrmContact =  frmContact;
  };
 
@@ -43,15 +44,27 @@ ButtonCons.prototype.newContact = function newContact() {
     let lastname = document.forms["newContactForm"]["lastName"].value;
     let phnumber = document.forms["newContactForm"]["phoneNumber"].value;
     let card = document.createElement("div");
+    let data = document.createElement("data");
     let contact = { Name: name,
                     Lastname: lastname,
                     Phone: phnumber};
 
     this.contact.push(contact);
+
+    
     card.classList.add("card");
-    card.innerHTML = "Name: " + contact.Name + '<br/>' + "Last Name: " + contact.Lastname + '<br/>' +"Phone Number: " + contact.Phone;
+    card.innerHTML = "Name: " + this.createData(contact.Name).outerHTML + '<br/>' + "Last Name: " + this.createData(contact.Lastname).outerHTML + '<br/>' +"Phone Number: " + this.createData(contact.Phone).outerHTML;
+    
+    
     this.pListContact.appendChild(card);
     
+}
+
+ButtonCons.prototype.createData = function createData (valore) {
+    let data = document.createElement('div')
+    data.classList.add("data");
+    data.innerHTML = valore;
+    return data;
 }
 
 ButtonCons.prototype.clear = function clear() {
